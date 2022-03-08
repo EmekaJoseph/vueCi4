bcd0b17a184f92c72494fd6609899f8bdcbf3b1b
emekaJoseph <josephiwuji@gmail.com>
2022-03-08 16:04:12 +0100

    todo correction

--- frontend/src/components/todoComponent.vue
+++ frontend/src/components/todoComponent.vue
@@ -1,75 +1,77 @@
 <template>
-    <div v-html="dom.topic"></div>
     <div>
-        <i class="small">{{ form.textInput }}</i>
-        <span class="float-end fw-bold badge rounded-pill bg-secondary">{{ itemsList.length }}</span> <br />
-        <br />
-        <div class="row gy-3">
-            <div class="col-md-6">
-                <form @submit.prevent>
-                    <input v-model.trim="form.textInput" type="text" placeholder="enter todo.." class="form-control"
-                        :class="{'is-invalid': form.textError}" />
-                </form>
-            </div>
-            <div class="col-md-4">
-                <DatepickerVue ref="dateInputRef" v-model="form.inputDate" :enableTimePicker="false"
-                    placeholder="Select Date" autoApply :state="form.dateError" />
-            </div>
-            <div class="col-md-2">
-                <button @click="addToList" class="btn btn-primary w-100">Add</button>
-            </div>
-        </div>
-        <!-- <br /> -->
-        <span class="row justify-content-center my-2" v-html="dom.alertmsg"></span>
-        <div class="p-5" style="background-color: #f5f5f0;">
-            <span v-if="itemsList.length">
-                <div class="table-responsive">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>S/N</th>
-                                <th>List Name</th>
-                                <th>Date</th>
-                                <th>Status</th>
-                                <th>Delete</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="(i, index) in itemsList" :key="i.Id">
-                                <th>{{ index + 1 }}.</th>
-                                <td>
-                                    <span :class="{ 'text-decoration-line-through': i.Done }">
-                                        {{i.Text}}
-                                    </span>
-                                </td>
-                                <td>
-                                    <span :class="{ 'text-decoration-line-through': i.Done }">
-                                        {{i.Date}}
-                                    </span>
-                                </td>
-                                <td>
-                                    <div class="form-check">
-                                        <input @change="updateLocalStorage" v-model="i.Done" class="form-check-input"
-                                            type="checkbox" :id="'name' + i.Id" />
-                                        <label class="form-check-label" :for="'name' + i.Id">
-                                            Completed
-                                        </label>
-                                    </div>
-                                </td>
-                                <td>
-                                    <button @click="removeItem(index)" class="btn p-0 px-2 btn-small"
-                                        :class="{'disabledBtn': !(i.Done), 'text-danger': i.Done}">delete
-                                    </button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+        <div v-html="dom.topic"></div>
+        <div>
+            <i class="small">{{ form.textInput }}</i>
+            <span class="float-end fw-bold badge rounded-pill bg-secondary">{{ itemsList.length }}</span> <br />
+            <br />
+            <div class="row gy-3">
+                <div class="col-md-6">
+                    <form @submit.prevent>
+                        <input v-model.trim="form.textInput" type="text" placeholder="enter todo.." class="form-control"
+                            :class="{'is-invalid': form.textError}" />
+                    </form>
                 </div>
-            </span>
-            <span v-else class="row justfy-content-center m-2">
-                <h3 class="text-center">List Is Empty</h3>
-                <small class="text-center">Enter list of TODOs</small>
-            </span>
+                <div class="col-md-4">
+                    <DatepickerVue ref="dateInputRef" v-model="form.inputDate" :enableTimePicker="false"
+                        placeholder="Select Date" autoApply :state="form.dateError" />
+                </div>
+                <div class="col-md-2">
+                    <button @click="addToList" class="btn btn-primary w-100">Add</button>
+                </div>
+            </div>
+            <!-- <br /> -->
+            <span class="row justify-content-center my-2" v-html="dom.alertmsg"></span>
+            <div class="p-5" style="background-color: #f5f5f0;">
+                <span v-if="itemsList.length">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>S/N</th>
+                                    <th>List Name</th>
+                                    <th>Date</th>
+                                    <th>Status</th>
+                                    <th>Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="(i, index) in itemsList" :key="i.Id">
+                                    <th>{{ index + 1 }}.</th>
+                                    <td>
+                                        <span :class="{ 'text-decoration-line-through': i.Done }">
+                                            {{i.Text}}
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <span :class="{ 'text-decoration-line-through': i.Done }">
+                                            {{i.Date}}
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <div class="form-check">
+                                            <input @change="updateLocalStorage" v-model="i.Done"
+                                                class="form-check-input" type="checkbox" :id="'name' + i.Id" />
+                                            <label class="form-check-label" :for="'name' + i.Id">
+                                                Completed
+                                            </label>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <button @click="removeItem(index)" class="btn p-0 px-2 btn-small"
+                                            :class="{'disabledBtn': !(i.Done), 'text-danger': i.Done}">delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </span>
+                <span v-else class="row justfy-content-center m-2">
+                    <h3 class="text-center">List Is Empty</h3>
+                    <small class="text-center">Enter list of TODOs</small>
+                </span>
+            </div>
         </div>
     </div>
 </template>
@@ -95,7 +97,7 @@
 
     const form = reactive({
         textInput: '',
-        inputDate: '',
+        inputDate: null,
         textError: false,
         dateError: null
     })
@@ -104,35 +106,40 @@
     const itemsList = ref([]);
     const dateInputRef = ref(null)
     const addToList = () => {
+        form.textError = false
+        form.dateError = null
         if (form.textInput.length < 3) {
             dom.alertmsg = `<span class="text-danger text-center">Enter TODO text not less than 3 Xters!</span>`
             form.textError = true
             return
         }
-        else {
-            form.textError = false
-            let todayDate = (new Date()).setHours(0, 0, 0, 0)
-            let inputDate_ = form.inputDate.setHours(0, 0, 0, 0)
-            console.log(form.inputDate);
-            if (form.inputDate === null || todayDate > inputDate_) {
-                dom.alertmsg = `<span class="text-danger text-center">Please input date (not earlier than today)!</span>`
-                form.dateError = false
-                return
-            }
-            else {
-                itemsList.value.push({
-                    Text: form.textInput,
-                    Date: customFormatDate(form.inputDate),
-                    Id: Date.now(),
-                    Done: false,
-                });
-                resetForm()
-                dom.alertmsg = `<span class="text-success text-center">Added to list Successfully!</span>`
-                setTimeout(() => {
-                    dom.alertmsg = ''
-                }, 1000);
 
-            }
+        if (form.inputDate == null) {
+            dom.alertmsg = `<span class="text-danger text-center">Please input date</span>`
+            form.dateError = false
+            return
+        }
+
+        let todayDate = (new Date()).setHours(0, 0, 0, 0)
+        let inputDate_ = form.inputDate.setHours(0, 0, 0, 0)
+        if (todayDate > inputDate_) {
+            dom.alertmsg = `<span class="text-danger text-center">Please input date (not earlier than today)!</span>`
+            form.dateError = false
+            return
+        }
+        else {
+            itemsList.value.push({
+                Text: form.textInput,
+                Date: customFormatDate(form.inputDate),
+                Id: Date.now(),
+                Done: false,
+            });
+            resetForm()
+            dom.alertmsg = `<span class="text-success text-center">Added to list Successfully!</span>`
+            setTimeout(() => {
+                dom.alertmsg = ''
+            }, 1000);
+
         }
 
     };
